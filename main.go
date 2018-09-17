8b94ae85b12b2d37218bd97b9c56e425290c3ca5
Luis Fernando Gaido <luisfernandogaido@gmail.com>
2018-09-17 20:59:29 -0300

    Primeira versão estável. Com argumentos.

--- main.go
+++ main.go
@@ -1,59 +1,92 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	n := 4
-	jumpsAllowed := []int{1, 2}
-
-	//numWays is the original solution of the problem, wich returns only the number of ways to reach the top.
-	numSolutions := numWays(n, jumpsAllowed)
-	fmt.Printf("number of solutions: %v\n", numSolutions)
-
-	//numWaysExtended shows all the paths too.
-	solutions := numWaysExtended(n, jumpsAllowed)
+	var (
+		n               int
+		jumpsAllowedStr string
+	)
+	flag.IntVar(&n, "n", 4, "Total steps")
+	flag.StringVar(&jumpsAllowedStr, "j", "1,3,5", "Jumps allowed separared by comma")
+	flag.Parse()
+	jumpsAllowed := make([]int, 0)
+	jumps := strings.Split(jumpsAllowedStr, ",")
+	for _, j := range jumps {
+		ja, err := strconv.Atoi(j)
+		if err != nil {
+			panic("all jumps must be integers")
+		}
+		jumpsAllowed = append(jumpsAllowed, ja)
+	}
+	fmt.Println("total steps:", n)
+	fmt.Println("jumps allowed:", jumpsAllowed)
+	solutions := numWays(n, jumpsAllowed)
 	fmt.Println("solutions:", len(solutions))
 	for _, solution := range solutions {
 		sum := 0
 		for _, step := range solution {
 			sum += step
 		}
-		fmt.Printf("%v: %v\n", solution, sum)
+		fmt.Printf("%v = %v\n", solution, sum)
 	}
 }
 
-func numWays(n int, x []int) int {
+func numWays(n int, x []int) [][]int {
 	validatePositives(n, x)
-	return up(n, removeRepetead(x), 0)
+	x = removeRepetead(x)
+	return up(n, x, 0, []int{})
 }
 
-func up(n int, x []int, level int) int {
-	solutions := 0
-	for _, steps := range x {
-		if level+steps == n {
-			solutions++
-		} else if level+steps < n {
-			solutions += up(n, x, level+steps)
-		}
-	}
-	return solutions
-}
-
-func numWaysExtended(n int, x []int) [][]int {
-	validatePositives(n, x)
-	return upExtended(n, removeRepetead(x), 0, []int{})
-}
-
-func upExtended(n int, x []int, level int, made []int) [][]int {
+func up(n int, x []int, level int, made []int) [][]int {
 	solutions := make([][]int, 0)
 	for _, steps := range x {
 		if level+steps == n {
 			solutions = append(solutions, append(made, steps))
 		} else if level+steps < n {
-			solutions = append(solutions, upExtended(n, x, level+steps, append(made, steps))...)
+			possibleSolutions := up(n, x, level+steps, append(made, steps))
+			for _, s := range possibleSolutions {
+				if sumSteps(s) == n {
+					solutions = append(solutions, s)
+				}
+			}
 		}
 	}
 	return solutions
+}
+
+func sumSteps(steps []int) int {
+	sum := 0
+	for _, step := range steps {
+		sum += step
+	}
+	return sum
+}
+
+func removeRepetead(s []int) []int {
+	m := make(map[int]bool)
+	uniques := make([]int, 0)
+	for _, e := range s {
+		if _, ok := m[e]; !ok {
+			uniques = append(uniques, e)
+			m[e] = true
+		}
+	}
+	return uniques
+}
+
+func validatePositives(n int, x []int) {
+	if n <= 0 {
+		panic("n must be a positive integer")
+	}
+	for _, steps := range x {
+		if steps <= 0 {
+			panic("all steps must be positive integers")
+		}
+	}
 }
